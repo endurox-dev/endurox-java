264265298be3dc799abdf7646bf0bfbf4d0378cf
Madars Vitolins <madars.vitolins@gmail.com>
2018-04-18 12:08:17 +0300

    Works on jni

--- libsrc/AtmiCtx.c
+++ libsrc/AtmiCtx.c
@@ -33,24 +33,25 @@ JNIEXPORT jobject JNICALL Java_org_endurox_AtmiCtx_getAtmiError (JNIEnv *env, jo
     NDRX_LOG(log_debug, "context: %ld (%p)", fieldVal, p_ctxt);
     err = Otperrno(p_ctxt);
     
+
     /* Get the class we wish to return an instance of */
-    jclass clazz = (*env)->FindClass(env, "org/endurox/ErrorTuple");
+    jclass errClazz = (*env)->FindClass(env, "org/endurox/ErrorTuple");
 
     /* Get the method id of an empty constructor in clazz */
-    jmethodID constructor = (*env)->GetMethodID(env, clazz, "<init>", "()V");
+    jmethodID constructor = (*env)->GetMethodID(env, errClazz, "<init>", "()V");
 
     /* Create an instance of clazz */
-    jobject obj = (*env)->NewObject(env, clazz, constructor);
+    jobject errObj = (*env)->NewObject(env, errClazz, constructor);
 
     /* Get Field references */
-    jfieldID param1Field = (*env)->GetFieldID(env, clazz, "err", "I");
-    jfieldID param2Field = (*env)->GetFieldID(env, clazz, "msg", "Ljava/lang/String;");
+    jfieldID param1Field = (*env)->GetFieldID(env, errClazz, "err", "I");
+    jfieldID param2Field = (*env)->GetFieldID(env, errClazz, "msg", "Ljava/lang/String;");
 
     /* Set fields for object */
-    (*env)->SetIntField(env, objClass, param1Field, err);
+    (*env)->SetIntField(env, errObj, param1Field, err);
 
-    jstr=(jstring)((*env)->NewStringUTF(Otpstrerror(p_ctxt, err)));
-    env->SetObjectField(objClass,param2Field,(jobject)jstr);
+    jstr=(jstring)((*env)->NewStringUTF(env, Otpstrerror(p_ctxt, err)) );
+    (*env)->SetObjectField(env, errObj,param2Field,(jobject)jstr);
 
     /* return object */
     return obj;
