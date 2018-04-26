24570f1c8bc0c86c3b3bad465eb044bd3d558fb3
Madars Vitolins <madars.vitolins@gmail.com>
2018-04-26 12:02:21 +0300

    Works on JAVA interface

--- libsrc/AtmiBuf.java
+++ libsrc/AtmiBuf.java
@@ -5,22 +5,56 @@ public class AtmiBuf {
   /**
    * Pointer to C ATMI Context object
    */
-   private Atmictx ctx;
-   private bool haveFinalizer;
+   private AtmiCtx ctx;
+   private boolean doFinalize;
    private long len;
    private long cPtr;
 
+   /**
+    * Free up the given buffer
+    * @param[in] cPtr C pointer to buffer
+    */
+   private native void tpfree (long cPtr);
+
+   /**
+    * Initialise ATMI Object
+    * @param ctx[in] ATMI Context allocated this method
+    * @param doFinalize[in] Should
+    * @param cPtr[in] C pointer to allocated block
+    * @param len[in] Conditional buffer length
+    */
+   public AtmiBuf(AtmiCtx ctx, boolean doFinalize, long cPtr, long len) {
+        ctx = ctx;
+        this.doFinalize = doFinalize;
+        this.cPtr = cPtr;
+        this.len = len;
+   }
+
+    /**
+     * Finish of this ATMI buffer
+     */
    @Override
    public void finalize() {
         //Call free from context?
         //Or call directly the c? I guess directly as context might be already
         //invalid
+        if (doFinalize)
+        {
+            tpfree(cPtr);
+        }
    }
+
+    /**
+     * Set the finalize flag
+     */
+    public void setDoFinalize(boolean b)
+    {
+        doFinalize = b;
+    }
 
    static {
       System.loadLibrary("exjava"); // Load native library at runtime
                            // hello.dll (Windows) or libenduroxjava.so (Unixes)
    }
-
 }
 
