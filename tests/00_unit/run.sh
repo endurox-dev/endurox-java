466fe6f9699fbc1a5e45b94b76e60085e38c7fee
Madars Vitolins <madars.vitolins@gmail.com>
2018-07-20 06:25:41 +0300

    Works on java linker

--- tests/00_unit/run.sh
+++ tests/00_unit/run.sh
@@ -5,21 +5,21 @@
 #
 
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../libsrc/c
-java -cp ../../libsrc/enduroxjava.jar:../libs/junit.jar:../libs/hamcrest-core.jar:./jexunit00.jar org.junit.runner.JUnitCore AtmiCtxTest
-
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../libsrc/c
+#java -cp ../../libsrc/enduroxjava.jar:../libs/junit.jar:../libs/hamcrest-core.jar:./jexunit00.jar org.junit.runner.JUnitCore AtmiCtxTest
 
 # Running with compiler;
 
 
 #tests/00_unit/run.sh
 #export LD_LIBRARY_PATH=/usr/lib/jvm/default-java/lib/:/usr/lib/jvm/default-java/lib/server/
-../../exjld/exjld -I ../../include -L ../../libexjlds -L/usr/lib/jvm/default-java/lib/ -L /usr/lib/jvm/default-java/lib/server/ -k -m org.junit.runner.JUnitCore ../../libsrc/enduroxjava.jar ../libs/junit.jar ../libs/hamcrest-core.jar ./jexunit00.jar
+#../../exjld/exjld -I ../../include -L ../../libexjlds -L/usr/lib/jvm/default-java/lib/ -L /usr/lib/jvm/default-java/lib/server/ -k -m org.junit.runner.JUnitCore ../../libsrc/enduroxjava.jar ../libs/junit.jar ../libs/hamcrest-core.jar ./jexunit00.jar
 
 #export LD_LIBRARY_PATH=/usr/lib/jvm/default-java/lib/:/usr/lib/jvm/default-java/lib/server/
 #export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 #./a.out AtmiCtxTest
 
 
-
+source ~/ndrx_home
+./jexunit00b AtmiCtxTest
 
