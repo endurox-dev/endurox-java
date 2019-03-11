c8cda7440d1b760281a39b6d0f3517ffa734e4fb
endurox-dev <contact@mavimax.com>
2019-03-11 18:31:20 +0200

    Works on notif testing

--- tests/01_basic_server/src/unit/TpNotifyTests.java
+++ tests/01_basic_server/src/unit/TpNotifyTests.java
@@ -9,10 +9,10 @@ import org.junit.Test;
  */
 public class TpNotifyTests implements UnsolCallback {
              
-    int nrstrings = 0 ;
+    int nrstring = 0 ;
     int nrjson = 0;
     int nrubf = 0 ;
-    int nrcarrays = 0;
+    int nrcarray = 0;
     int nrnull = 0;
     int nrview = 0;
     
@@ -27,17 +27,23 @@ public class TpNotifyTests implements UnsolCallback {
         TpTypesResult t = buf.tptypes();
         
         if (t.getType().equals("STRING")) {
-            nrstrings++;
+            TypedString s = (TypedString)buf;
+            /* Check the value sent in... */
+            String ss = s.getString();
+            assertEquals("HELLO NOTIF", ss);
+            nrstring++;
         } else if (t.getType().equals("JSON")) {
+            TypedJson j = (TypedJson)buf;
+            /* Check the value sent in... */
+            String js = j.getJSON();
+            assertEquals("{}", js);
             nrjson++;
         } else if (t.getType().equals("UBF")) {
             TypedUbf ub = (TypedUbf)buf;
-            
             /* Check the value sent in... */
             String s = ub.BgetString(test.T_STRING_10_FLD, 5);
             assertEquals("HELLO UBF FROM SERVICE", s);
             nrubf++;
-            
         }
     }
     
@@ -46,9 +52,6 @@ public class TpNotifyTests implements UnsolCallback {
         
         AtmiCtx ctx = new AtmiCtx();
         assertNotEquals(ctx.getCtx(), 0x0);
-
-        TypedUbf ub = (TypedUbf)ctx.tpalloc("UBF", "", 1024);
-        assertNotEquals(ub, null);
         
         boolean leaktest = false;
         int leaktestSec = 0;
@@ -77,12 +80,12 @@ public class TpNotifyTests implements UnsolCallback {
         for (int i=0; ((i<1000) || (leaktest && w.deltaSec() < leaktestSec)); i++)
         {
                          
-            nrstrings = 0 ;
-            nrjson = 0;
-            nrubf = 0 ;
-            nrcarrays = 0;
             nrnull = 0;
+            nrstring = 0 ;
+            nrjson = 0;
+            nrcarray = 0;
             nrview = 0;
+            nrubf = 0 ;
             /* loop over the buffer types
              * send them to server and expect one to be received back..
              * each one we shall test with:
@@ -94,14 +97,43 @@ public class TpNotifyTests implements UnsolCallback {
              * - test binary name not matched (0 bordcasts)
              * - test binary named matched by regexp (i.e. flags test).
              */
-            String reqData = String.format("loop %d", i);
-            ub.Bchg(test.T_STRING_FLD, 0, String.format("loop %d", i));
+
+            ctx.tplogInfo("*** NULL test *** ");
+            ctx.tpcall("GLOBNOTIF", null, 0);
+            assertEquals(1, nrnull);
             
-            ub = (TypedUbf)ctx.tpcall("GLOBNOTIF", ub, 0);
+            ctx.tplogInfo("*** STRING test ***");
+            TypedString s = (TypedString)ctx.tpalloc("STRING", "", 1024);
+            assertNotEquals(s, null);            
+            ctx.tpcall("GLOBNOTIF", s, 0);
+            assertEquals(1, nrstring);
             
+            ctx.tplogInfo("*** JSON test ***");
+            TypedJson j = (TypedJson)ctx.tpalloc("JSON", "", 1024);
+            assertNotEquals(j, null);            
+            ctx.tpcall("GLOBNOTIF", j, 0);
+            assertEquals(1, nrjson);
+            
+            ctx.tplogInfo("*** CARRAY test ***");
+            TypedCarray c = (TypedCarray)ctx.tpalloc("CARRAY", "", 1024);
+            assertNotEquals(c, null);            
+            ctx.tpcall("GLOBNOTIF", c, 0);
+            assertEquals(1, nrcarray);
+            
+            ctx.tplogInfo("*** VIEW test ***");
+            TypedView v = (TypedView)ctx.tpalloc("VIEW", "JVIEW1", 1024);
+            assertNotEquals(c, null);            
+            ctx.tpcall("GLOBNOTIF", v, 0);
+            assertEquals(1, nrview);
+            
+            ctx.tplogInfo("*** UBF test ***");
+            TypedUbf ub = (TypedUbf)ctx.tpalloc("UBF", "", 1024);
+            assertNotEquals(ub, null);            
+            ctx.tpcall("GLOBNOTIF", ub, 0);
             assertEquals(1, nrubf);
+            
         }
-        ub.cleanup();
+
         ctx.cleanup();
     }
     
