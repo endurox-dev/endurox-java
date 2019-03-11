793edb0383450a7205fe39434264d493a45000c7
endurox-dev <contact@mavimax.com>
2019-03-11 08:20:32 +0200

    Notify tests

---
+++ tests/01_basic_server/src/unit/TpNotifyTests.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import org.endurox.*;
+import org.endurox.exceptions.AtmiException;
+
+/**
+ * Perform tpnotify tests
+ */
+public class TpNotifyTests implements UnsolCallback {
+             
+    int nrstrings = 0 ;
+    int nrjson = 0;
+    int nrubf = 0 ;
+    int nrcarrays = 0;
+    int nrnull = 0;
+    int nrview = 0;
+    
+    /**
+     * Receive different types of messages
+     * @param ctx Atmi context
+     * @param buf typed buffer received
+     * @param flags flags
+     */
+    public void unsolCallback(AtmiCtx ctx, TypedBuffer buf, long flags)
+    {
+        
+    }
+    
+}
