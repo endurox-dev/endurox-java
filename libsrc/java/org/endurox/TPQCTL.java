f380f5b529a84ac7a6b6ee12ffd176ae0f7d8b65
endurox-dev <contact@mavimax.com>
2019-03-22 10:37:36 +0200

    Added setters & getters

--- libsrc/java/org/endurox/TPQCTL.java
+++ libsrc/java/org/endurox/TPQCTL.java
@@ -38,6 +38,126 @@ package org.endurox;
  * Holds infos about message to be enqueued or dequeued
  */
 public class TPQCTL {
+
+    public long getFlags() {
+        return flags;
+    }
+
+    public void setFlags(long flags) {
+        this.flags = flags;
+    }
+
+    public long getDeq_time() {
+        return deq_time;
+    }
+
+    public void setDeq_time(long deq_time) {
+        this.deq_time = deq_time;
+    }
+
+    public long getPriority() {
+        return priority;
+    }
+
+    public void setPriority(long priority) {
+        this.priority = priority;
+    }
+
+    public long getDiagnostic() {
+        return diagnostic;
+    }
+
+    public void setDiagnostic(long diagnostic) {
+        this.diagnostic = diagnostic;
+    }
+
+    public String getDiagmsg() {
+        return diagmsg;
+    }
+
+    public void setDiagmsg(String diagmsg) {
+        this.diagmsg = diagmsg;
+    }
+
+    public byte[] getMsgid() {
+        return msgid;
+    }
+
+    public void setMsgid(byte[] msgid) {
+        this.msgid = msgid;
+    }
+
+    public byte[] getCorrid() {
+        return corrid;
+    }
+
+    public void setCorrid(byte[] corrid) {
+        this.corrid = corrid;
+    }
+
+    public String getReplyqueue() {
+        return replyqueue;
+    }
+
+    public void setReplyqueue(String replyqueue) {
+        this.replyqueue = replyqueue;
+    }
+
+    public String getFailurequeue() {
+        return failurequeue;
+    }
+
+    public void setFailurequeue(String failurequeue) {
+        this.failurequeue = failurequeue;
+    }
+
+    public ClientId getCltid() {
+        return cltid;
+    }
+
+    public void setCltid(ClientId cltid) {
+        this.cltid = cltid;
+    }
+
+    public long getUrcode() {
+        return urcode;
+    }
+
+    public void setUrcode(long urcode) {
+        this.urcode = urcode;
+    }
+
+    public long getAppkey() {
+        return appkey;
+    }
+
+    public void setAppkey(long appkey) {
+        this.appkey = appkey;
+    }
+
+    public long getDelivery_qos() {
+        return delivery_qos;
+    }
+
+    public void setDelivery_qos(long delivery_qos) {
+        this.delivery_qos = delivery_qos;
+    }
+
+    public long getReply_qos() {
+        return reply_qos;
+    }
+
+    public void setReply_qos(long reply_qos) {
+        this.reply_qos = reply_qos;
+    }
+
+    public long getExp_time() {
+        return exp_time;
+    }
+
+    public void setExp_time(long exp_time) {
+        this.exp_time = exp_time;
+    }
 	
     long flags;             /**< indicates which of the values are set */
     long deq_time;          /**< absolute/relative  time for dequeuing */
