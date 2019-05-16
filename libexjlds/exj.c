9c8a9af768873aaf5b4adb34d7faef482cc46dc4
Madars Vitolins <madars.vitolins@gmail.com>
2019-05-16 12:03:14 +0300

    Works on java interface

--- libexjlds/exj.c
+++ libexjlds/exj.c
@@ -388,6 +388,213 @@ out:
     return ret;
 }
 
+#if 0
+/* save the original pointer... */
+__thread void * old_malloc_hook;
+
+static void *
+my_malloc_hook (size_t size, const void *caller)
+{
+  void *result;
+  long ldate, ltime, lusec;
+  nstd_tls_t *tls;
+  /* Restore all old hooks */
+  __malloc_hook = old_malloc_hook;
+  
+  /* Call recursively */
+  result = malloc (size);
+  
+  /* Save underlying hooks */
+  old_malloc_hook = __malloc_hook;
+  
+  /* printf might call malloc, so protect it too. 
+  printf ("%08ld:%06ld%06d: malloc (%d) returns %p\n", ldate, ltime, lusec, 
+          (int)size, result);
+  
+  ndrx_get_dt_local(&ldate, &ltime, &lusec);
+  */
+  
+  /*
+  userlog("malloc (%d) returns %p\n",
+          (int)size, result);
+  */
+  
+  NDRX_LOG(log_error, "malloc (%d) returns %p", (int)size, result);
+  
+  /* print some log only if we have a Enduro/X TLS... */
+
+  /* Restore our own hooks */
+  __malloc_hook = my_malloc_hook;
+  
+  return result;
+}
+
+static void
+my_init (void)
+{
+  old_malloc_hook = __malloc_hook;
+  __malloc_hook = my_malloc_hook;
+}
+
+
+#endif
+
+/**
+ * We need to split up the source so that
+ * - we create JVM
+ * - get static method for current thread (assoc the context)
+ * See this:
+ * https://stackoverflow.com/questions/26256558/calling-jni-createjavavm-function-twice
+ * so we have single env, but may be assoc'd with multiple threads, which have
+ * proper context set?
+ */
+
+
+/**
+ * Get static handler/factory method of the object
+ * @param env java env
+ * @param run_class class name to run
+ * @param static_method method name to run
+ * @param static_method_sign signature of the method
+ * @param run_class ptr to class
+ * @param run_mid ptr to method id
+ * @return EXSUCCEED/EXFAIL
+ */
+exprivate int ndrxj_ldr_get_static_handler(JNIEnv *env, 
+			char *run_class,
+                        char *static_method,
+                        char *static_method_sign,
+                        jclass *run_class,
+                        jmethodID *run_mid
+  			)
+{
+    int ret = EXSUCCEED;
+    jmethodID load_class;
+    jclass thread_class;
+    jmethodID cur_thread_mid;
+    jobject cur_thread;
+    jmethodID set_ctx_mid;
+
+    /* boot the main method of the class */
+
+    load_class  = (*env)->GetMethodID(env, M_classLoaderClass, 
+                            "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
+    if(NULL==load_class)
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                            "Cannot get method 'findClass' of StreamLoader: %s");
+        EXFAIL_OUT(ret);
+    }
+
+    /* get the main class */
+    *run_class = (jclass) (*env)->CallObjectMethod(env, M_classLoader, 
+                                                   load_class, 
+                                            (*env)->NewStringUTF(env, run_class));
+    if (NULL==*run_class)
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                            "Failed to get main class: %s");
+        EXFAIL_OUT(ret);
+    }
+
+    /* get main method... */
+
+    *run_mid = (*env)->GetStaticMethodID(env, *run_class, static_method, 
+                                    /*"([Ljava/lang/String;)V"*/static_method_sign); 
+
+    if (*run_mid == 0)
+    { 
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                            "Failed to get main method: %s");
+        EXFAIL_OUT(ret);
+    }
+
+    
+    /* set current class loader? */
+    thread_class = (*env)->FindClass(env, "java/lang/Thread");
+    
+    if (NULL==thread_class)
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                "Failed to find Thread class: %s");
+        EXFAIL_OUT(ret);
+    }
+    
+    cur_thread_mid = (*env)->GetStaticMethodID(env, thread_class, 
+            "currentThread", "()Ljava/lang/Thread;");
+    
+    if (NULL==cur_thread_mid)
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                "Failed to get currentThread() mid: %s");
+        EXFAIL_OUT(ret);
+    }
+    
+    cur_thread = (*env)->CallStaticObjectMethod(env, thread_class, cur_thread_mid);
+    
+    if (NULL==cur_thread)
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                "Failed to get current thread: %s");
+        EXFAIL_OUT(ret);
+    }
+    
+    set_ctx_mid = (*env)->GetMethodID(env, thread_class, 
+            "setContextClassLoader", "(Ljava/lang/ClassLoader;)V");
+    
+    if (NULL==set_ctx_mid)
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                "Failed to get setContextClassLoader() mid: %s");
+        EXFAIL_OUT(ret);
+    }
+    
+    (*env)->CallVoidMethod(env, cur_thread, set_ctx_mid, M_classLoader);
+    
+    
+    if((*env)->ExceptionCheck(env))
+    {
+        EXJLD_LOG_EXCEPTION(env, log_error, 
+                        "Failed to run main: %s (%s)",
+                        main_class_str);
+        (*env)->ExceptionClear(env);
+        EXFAIL_OUT(ret);
+    }
+
+#if
+    if (!test_mode)
+    {
+        NDRX_LOG(log_debug, "Starting...");
+        /* boot up... */
+        (*env)->CallStaticVoidMethod(env, main_class, mid, args);
+
+
+        if((*env)->ExceptionCheck(env))
+        {
+            EXJLD_LOG_EXCEPTION(env, log_error, 
+                            "Failed to run main: %s (%s)",
+                            main_class_str);
+            (*env)->ExceptionClear(env);
+            EXFAIL_OUT(ret);
+        }
+
+    }
+#endif
+    
+	
+out:
+    
+    if (NULL!=thread_class)
+    {
+        (*env)->DeleteLocalRef( env, thread_class);
+    }
+
+NDRX_LOG(log_debug, "%s returns  %d", __func__, ret);
+
+    return ret;
+}
+
+
 /**
  * Run the loader 
  * For exjdbc we could call a static method
@@ -568,252 +775,18 @@ out:
     return ret;
 }
 
-#if 0
-/* save the original pointer... */
-__thread void * old_malloc_hook;
-
-static void *
-my_malloc_hook (size_t size, const void *caller)
-{
-  void *result;
-  long ldate, ltime, lusec;
-  nstd_tls_t *tls;
-  /* Restore all old hooks */
-  __malloc_hook = old_malloc_hook;
-  
-  /* Call recursively */
-  result = malloc (size);
-  
-  /* Save underlying hooks */
-  old_malloc_hook = __malloc_hook;
-  
-  /* printf might call malloc, so protect it too. 
-  printf ("%08ld:%06ld%06d: malloc (%d) returns %p\n", ldate, ltime, lusec, 
-          (int)size, result);
-  
-  ndrx_get_dt_local(&ldate, &ltime, &lusec);
-  */
-  
-  /*
-  userlog("malloc (%d) returns %p\n",
-          (int)size, result);
-  */
-  
-  NDRX_LOG(log_error, "malloc (%d) returns %p", (int)size, result);
-  
-  /* print some log only if we have a Enduro/X TLS... */
-
-  /* Restore our own hooks */
-  __malloc_hook = my_malloc_hook;
-  
-  return result;
-}
-
-static void
-my_init (void)
-{
-  old_malloc_hook = __malloc_hook;
-  __malloc_hook = my_malloc_hook;
-}
-
-
-#endif
-
-/**
- * We need to split up the source so that
- * - we create JVM
- * - get static method for current thread (assoc the context)
- * See this:
- * https://stackoverflow.com/questions/26256558/calling-jni-createjavavm-function-twice
- * so we have single env, but may be assoc'd with multiple threads, which have
- * proper context set?
- */
-
-
-
-exprivate int ndrxj_ldr_get_static_handler(JNIEnv *env, 
-			char *static_class,
-                        char *static_method,
-			int argc, char **argv,
-			int test_mode
-  			)
-{
-    int ret = EXSUCCEED;
-    jclass main_class;
-    jclass str_class;
-    jmethodID mid;
-
-    jstring jstr; 
-    jobjectArray args; 
-    int i;
-    jmethodID load_class;
-    
-    jclass thread_class;
-    jmethodID cur_thread_mid;
-    jobject cur_thread;
-    jmethodID set_ctx_mid;
-
-    /* boot the main method of the class */
-
-    load_class  = (*env)->GetMethodID(env, M_classLoaderClass, 
-                            "loadClass", "(Ljava/lang/String;)Ljava/lang/Class;");
-    if(NULL==load_class)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                            "Cannot get method 'findClass' of StreamLoader: %s");
-        EXFAIL_OUT(ret);
-    }
-
-    /* get the main class */
-    main_class = (jclass) (*env)->CallObjectMethod(env, M_classLoader, 
-                                                   load_class, 
-                                            (*env)->NewStringUTF(env, main_class_str));
-    if (NULL==main_class)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                            "Failed to get main class: %s");
-        EXFAIL_OUT(ret);
-    }
-
-    /* get main method... */
-
-    mid = (*env)->GetStaticMethodID(env, main_class, "main", 
-                                    "([Ljava/lang/String;)V"); 
-
-    if (mid == 0)
-    { 
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                            "Failed to get main method: %s");
-        EXFAIL_OUT(ret);
-    }
-
-    /* fire it up! */
-
-    /*
-     * Build CLI..
-     */
-    str_class = (*env)->FindClass(env, "java/lang/String");
-
-    if (NULL==str_class)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                "Cannot find String Class: %s");
-        EXFAIL_OUT(ret);
-    }
-
-    args = (*env)->NewObjectArray(env, argc, str_class, jstr); 
-
-    if (NULL==args)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                "Failed to allocate command line arguments: %s");
-        EXFAIL_OUT(ret);
-    }
-
-    for (i=0; i<argc; i++)
-    {
-        jstring argString = (*env)->NewStringUTF(env, argv[i]);
-
-        if (NULL==argString)
-        {	
-            EXJLD_LOG_EXCEPTION(env, log_error, 
-                    "Failed to allocate cli arguments: %s (i=%d, argc=%d)",
-                    i, argc);
-            EXFAIL_OUT(ret);
-        }
-
-        /* set array elm */
-        (*env)->SetObjectArrayElement(env, args, i, (jobject)argString);
-
-        /* Check exception.. */
-        if((*env)->ExceptionCheck(env))
-        {
-            EXJLD_LOG_EXCEPTION(env, log_error, 
-                            "Failed to set args array elment: %s "
-                            "(i=%d, argc=%d)",
-                            i, argc);
-            EXFAIL_OUT(ret);
-        }
-
-    }
-	
-    /* set current class loader? */
-    thread_class = (*env)->FindClass(env, "java/lang/Thread");
-    
-    if (NULL==thread_class)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                "Failed to find Thread class: %s");
-        EXFAIL_OUT(ret);
-    }
-    
-    cur_thread_mid = (*env)->GetStaticMethodID(env, thread_class, 
-            "currentThread", "()Ljava/lang/Thread;");
-    
-    if (NULL==cur_thread_mid)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                "Failed to get currentThread() mid: %s");
-        EXFAIL_OUT(ret);
-    }
-    
-    cur_thread = (*env)->CallStaticObjectMethod(env, thread_class, cur_thread_mid);
-    
-    if (NULL==cur_thread)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                "Failed to get current thread: %s");
-        EXFAIL_OUT(ret);
-    }
-    
-    set_ctx_mid = (*env)->GetMethodID(env, thread_class, 
-            "setContextClassLoader", "(Ljava/lang/ClassLoader;)V");
-    
-    if (NULL==set_ctx_mid)
-    {
-        EXJLD_LOG_EXCEPTION(env, log_error, 
-                "Failed to get setContextClassLoader() mid: %s");
-        EXFAIL_OUT(ret);
-    }
-    
-    (*env)->CallVoidMethod(env, cur_thread, set_ctx_mid, M_classLoader);
-
-    if (!test_mode)
-    {
-        NDRX_LOG(log_debug, "Starting...");
-        /* boot up... */
-        (*env)->CallStaticVoidMethod(env, main_class, mid, args);
-
-
-        if((*env)->ExceptionCheck(env))
-        {
-            EXJLD_LOG_EXCEPTION(env, log_error, 
-                            "Failed to run main: %s (%s)",
-                            main_class_str);
-            (*env)->ExceptionClear(env);
-            EXFAIL_OUT(ret);
-        }
-
-    }
-	
-out:
-    NDRX_LOG(log_debug, "%s returns  %d", __func__, ret);
-
-    return ret;
-}
-
 /**
  * Get java virtual machine instance
  * NOTE ! Only single instance per process!
  * @param class_index array of classes
  * @param class_index_len array len
- * @param emb_index resources array
- * @param emb_index_len res array len
+ * @param res_index resources array
+ * @param res_index_len res array len
  * @param[out] env allocated env for current rhead
  * @return ptr to Java VM or NULL on failure
  */
 expublic JavaVM * ndrxj_ldr_getvm(ndrxj_class_index_t *class_index, 
-	        int class_index_len, ndrxj_class_index_t *emb_index, int emb_index_len,
+	        int class_index_len, ndrxj_class_index_t *res_index, int res_index_len,
             JNIEnv **env)
 {
     JavaVM *vm = NULL;
@@ -829,6 +802,12 @@ expublic JavaVM * ndrxj_ldr_getvm(ndrxj_class_index_t *class_index,
     
     NDRX_LOG(log_debug, "Configuring Java VM");
     
+    
+    M_class_index = class_index;
+    M_class_index_len = class_index_len;
+    
+    M_res_index = res_index;
+    M_res_index_len = res_index_len;
     
         /* Load Enduro/X based config... with @java section */
     if (EXSUCCEED!=ndrx_cconfig_load_general(&cfg))
